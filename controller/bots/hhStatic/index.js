36eb429fc49ac41306fb64e4828c7e79e2dc6b5c
bobacCurry <bobac.curry@gmail.com>
2022-03-12 20:49:59 +0800

    fix

--- controller/bots/hhStatic/index.js
+++ controller/bots/hhStatic/index.js
@@ -1,12 +1,16 @@
 const axios = require('axios')
 
+const ROLELIST = ['admin','','']
+
 module.exports = {
 	
 	index: async (req, res, next) =>{
 
 		const token = req.params.token
 
-		console.log(req.body)
+		const { update_id, message: { message_id, from, chat, text, entities } } = req.body
+
+		console.log(message_id, from, chat, text, entities)
 
 		return res.send('true')
 	}
