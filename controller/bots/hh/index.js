565a7b18d86aa1c970a20e26baee9e63e783aad4
bobacCurry <bobac.curry@gmail.com>
2022-04-22 22:16:52 +0800

    fix

--- controller/bots/hh/index.js
+++ controller/bots/hh/index.js
@@ -1005,7 +1005,7 @@ const getWater = async (token,message_id,from,chat,text,all=false) => {
 			}
 		}
 
-		let water_text = '入款（' + count_in + '笔）：\n'+ water_in.join('\n') + '\n出款（' + count_out + '笔）：\n' + water_out.join('\n') + '\n费率：' + charge + '\n' + rate_array.join('\n') + '\n总入款：' + in_total + '人民币\n应下发：' + out_should_array.join(' | ') + '\n总下发：' + out_total_array.join(' | ') + '\n余下发：' + out_need_array.join(' | ')
+		let water_text = '入款（' + count_in + '笔）：\n'+ water_in.join('\n') + '\n\n出款（' + count_out + '笔）：\n' + water_out.join('\n') + '\n\n费率：' + charge + '\n' + rate_array.join('\n') + '\n\n总入款：' + in_total + '人民币\n\n应下发：' + out_should_array.join(' | ') + '\n\n总下发：' + out_total_array.join(' | ') + '\n\n余下发：' + out_need_array.join(' | ')
 
 		await API.sendMessage(token, { chat_id: cid, text: water_text })
 
