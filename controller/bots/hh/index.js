38737c164a1637939658ceb630960dd21e991371
bobacCurry <bobac.curry@gmail.com>
2022-04-22 22:51:53 +0800

    fix

--- controller/bots/hh/index.js
+++ controller/bots/hh/index.js
@@ -1003,7 +1003,7 @@ const getWater = async (token,message_id,from,chat,text,all=false) => {
 
 		let water_text = '入款（' + count_in + '笔）：\n'+ water_in.join('\n') + '\n\n出款（' + count_out + '笔）：\n' + water_out.join('\n') + '\n\n费率：' + charge + '\n' + rate_array.join('\n') + '\n总入款：' + in_total + '人民币\n应下发：' + out_should_array.join(' | ') + '\n总下发：' + out_total_array.join(' | ') + '\n余下发：' + out_need_array.join(' | ')
 
-		await API.sendMessage(token, { chat_id: cid, parse_mode: 'HTML', text: water_text })
+		await API.sendMessage(token, { chat_id: cid, parse_mode: 'HTML', text: water_text, reply_markup: { inline_keyboard: [ [ { text:'获取完整账单', callback_data:'/bill' } ] ] } })
 
 	}catch(err){
 
